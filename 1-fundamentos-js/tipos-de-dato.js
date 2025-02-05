993fd1ee93103464c0106631470026df74340aa2
erickha16 <errickha@gmail.com>
2025-02-04 20:55:10 -0600

    Tipos de datos en JS

---
+++ 1-fundamentos-js/tipos-de-dato.js
@@ -0,0 +1,29 @@
+console.log(typeof "redonda");
+
+console.log("Hola mundo");
+
+console.log("El tipo de dato de false es: " + typeof false);
+
+console.log("_______________________________________________________");
+console.log("EJEMPLOS DE DIFERENTES TIPOS DE DATOS:");
+
+console.log("EJEMPLO: Tipo de dato");
+console.log("42: " + typeof 42);
+console.log("'Veinticinco': " + typeof "Veinticinco");
+console.log("-66: " + typeof -66);
+console.log("true: " + typeof true);
+console.log("0: " + typeof 0);
+console.log("''" + typeof "");
+console.log("null: " + typeof null);
+console.log("undefined: " + typeof undefined);
+console.log("false: " + typeof false);
+
+console.log("_______________________________________________________");
+console.log("Extras:");
+
+console.log("23 + 27: " + typeof (23 + 27));
+console.log("El perro no dormía bien: " + typeof "El perro no dormía bien");
+let comida;
+console.log(
+  "Variable definida de la siguiente manera 'let comida': " + typeof comida
+);
