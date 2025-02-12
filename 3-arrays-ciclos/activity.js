4e3e00b2949c030548b7b43ddbce432616df2c21
erickha16 <errickha@gmail.com>
2025-02-11 23:25:31 -0600

    Actividad arrays-ciclos

--- 3-arrays-ciclos/activity.js
+++ 3-arrays-ciclos/activity.js
@@ -102,11 +102,70 @@ let frutas = [
   },
 ];
 
+/*
 for (let i = 0; i < frutas.length; i++) {
   const element = frutas[i];
-  console.log(`${i + 1}. ${element.nombre}`);
+  //   console.log(`${i + 1}. ${element.nombre}`);
 
-  if (element.color === "verde") {
-    console.log(element.nombre + " 💚 ");
+  //   if (element.color === "verde") {
+  //     console.log(element.nombre + " 💚 ");
+  //   }
+}
+*/
+
+/*
+//Tipos de fruta presentes
+let tipoDeFrutas = [];
+for (let i = 0; i < frutas.length; i++) {
+  const element = frutas[i];
+  if (!tipoDeFrutas.includes(element.nombre)) {
+    tipoDeFrutas.push(element.nombre);
   }
 }
+
+
+
+console.log("Los tipos de frutas disponobles son:");
+for (let i = 0; i < tipoDeFrutas.length; i++) {
+  console.log(`${i + 1}.- ${tipoDeFrutas[i]}`);
+}
+
+*/
+
+//Tipos de fruta presentes y sus cantidades
+let frutasEnExistencia = []; //Arreglo para guardar los tipos de frutas y sus cantidades
+let tipoDeFrutas = []; //Arreglo para guardar únicamnete los tipos de frutas
+
+//Contador del while
+contador = 0;
+//Ciclo para recorrer el arreglode todas las frutas
+//(Personalmente, me acomodaría utilizando mejor un doble for, pero por cuestiones de aprendizaje, utilicé ambos ciclos)
+while (contador < frutas.length) {
+  const element = frutas[contador];
+
+  //Si la fruta aún no se encuentra dentro del arreglo "tipoDeFrutas", entra en el if
+  if (!tipoDeFrutas.includes(element.nombre)) {
+    let cont = 0; //Inicializa el contador para saber la cantidad de dicha fruta
+
+    //Bucle para saber cuantas frutas de ese tipo hay dentro del arreglo
+    for (let i = 0; i < frutas.length; i++) {
+      const fruta = frutas[i];
+      if (element.nombre == fruta.nombre) {
+        cont++; //Suma 1 por cada vez que coincida el nombre
+      }
+    }
+
+    //constante creada para almacenar el tipo de fruta y su cantidad.
+    const data = {
+      nombre: element.nombre,
+      cantidad: cont,
+    };
+
+    tipoDeFrutas.push(data.nombre); //Se agrega el nombre de la fruta al arreglo para evitar que se repita
+    frutasEnExistencia.push(data); //Se suma data al arreglo objetivo
+  }
+
+  contador++;
+}
+//Imprimimos las frutas en existenica (Nombre y cantidad)
+console.log(frutasEnExistencia);
